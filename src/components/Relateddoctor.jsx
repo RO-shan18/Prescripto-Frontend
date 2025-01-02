2381cf00064cdf5b2d466f94f5e425ba7365af98
RO-shan18 <mehraroshan1802@gmail.com>
2025-01-02 16:46:44 +0530

    Add responsiveness to the doctor's appointment page

--- src/components/Relateddoctor.jsx
+++ src/components/Relateddoctor.jsx
@@ -20,20 +20,20 @@ const Relateddoctor = ({docId, speciality}) => {
   return (
     <div className="flex flex-col items-center mt-20 w-4/5 m-auto gap-12">
       <div className="flex flex-col items-center gap-3">
-        <h1 className="text-3xl font-semibold">Related Doctors</h1>
-        <p className="text-sm">Simply browse through our extensive list of trusted doctors.</p>
+        <h1 className="text-xl md:text-3xl font-semibold">Related Doctors</h1>
+        <p className="text-[12px] text-center md:text-left md:text-sm">Simply browse through our extensive list of trusted doctors.</p>
       </div>
-      <div className="flex flex-wrap gap-10 ">
+      <div className="flex flex-wrap md:justify-start md:items-start justify-center items-center gap-10 ">
         {reldoctor.slice(0,5).map((data) => {
           return (
           <div key={data._id} onClick={()=>{ Navigate("/Appointments/" + data._id); scrollTo(0,0);}} className="flex flex-col items-start bg-gray-200 rounded-xl p-2 gap-2 hover:translate-y-[-10px] transition-all duration-500 cursor-pointer">
-             <img className="w-48" src={data.image}/>
-             <div><div className="flex gap-2 items-center text-sm">
+             <img className="w-36 md:w-48" src={data.image}/>
+             <div><div className="flex gap-2 items-center text-[12px] md:text-sm">
                 <p className="h-2 w-2 rounded-full bg-green-500"></p>
                 <p className="text-green-500">Available</p>
              </div>
-             <p className="font-semibold">{data.name}</p>
-             <p className="text-sm">{data.speciality}</p></div>
+             <p className="font-semibold text-[12px] md:text-sm">{data.name}</p>
+             <p className="text-[12px] md:text-sm">{data.speciality}</p></div>
           </div>
           );
         })}
--- src/pages/Appointments.jsx
+++ src/pages/Appointments.jsx
@@ -75,55 +75,55 @@ const Appointments = () => {
 
   return (
     <div className="w-[100vw]">
-    <div className="flex w-4/5 m-auto gap-6">
+    <div className="flex w-4/5 m-auto gap-6 md:flex-row flex-col md:items-start md:justify-start items-center justify-center">
       <div>
-        <img className="w-80 bg-primary rounded-lg" src={docinfo.image} />
+        <img className="w-[100%] md:w-80 bg-primary rounded-lg" src={docinfo.image} />
       </div>
-      <div className="w-4/5 flex flex-col gap-2 items-start border border-gray-400 py-5 px-10 rounded-lg">
-      <h1 className="text-3xl">{docinfo.name}</h1>
-      <div className="flex gap-3 items-center">
-        <p className="text-sm text-gray-500">
+      <div className=" md:w-4/5 flex flex-col gap-1 lg:gap-2 items-start border border-gray-400 pt-3 lg:py-5 px-5 lg:px-10 rounded-lg">
+      <h1 className="text-xl lg:text-3xl">{docinfo.name}</h1>
+      <div className="flex gap-2 lg:gap-3 items-center">
+        <p className="text-[12px] lg:text-sm text-gray-500">
           {docinfo.degree} - {docinfo.speciality}
         </p>
-        <button className="border border-gray-500 px-4 py-1 rounded-full text-sm">{docinfo.experience}</button>
+        <button className="border border-gray-500 px-4 py-1 rounded-full text-[12px] lg:text-sm">{docinfo.experience}</button>
       </div>
       <div>
-        <p className="flex gap-3 text-sm">About <img src={assets.info_icon}/></p>
-        <p className="text-gray-500 text-sm">{docinfo.about}</p>
+        <p className="flex gap-2 text-[12px] lg:gap-3 lg:text-sm">About <img src={assets.info_icon}/></p>
+        <p className="text-gray-500 text-[12px] lg:text-sm">{docinfo.about}</p>
       </div>
       <div className="flex gap-1">
-        <p className="text-gray-500">Appointment fees: 
+        <p className="text-gray-500 lg:text-lg text-[12px]">Appointment fees: 
          </p>
-        <p className="text-sm">{currency}
+        <p className="text-[12px] lg:text-sm">{currency}
         {docinfo.fees}</p>
       </div>
       </div>
     </div>
-     <div className="flex flex-col items-start justify-start w-6/12 ml-[468px] mt-12 gap-6">
-      <h1 className="text-gray-500">Booking Slots</h1>
-      <div className="flex flex-col gap-6">
-      <div className="flex gap-3">
+     <div className="flex flex-col w-4/5 justify-center md:justify-normal md:w-2/6 mx-auto md:mx-auto mt-12 gap-6">
+      <h1 className="text-gray-500 md:text-xl text-lg md:text-left text-center">Booking Slots</h1>
+      <div className="flex flex-col md:justify-normal justify-center items-center md:items-start gap-6">
+      <div className="flex gap-3 w-5/6 sm:overflow-visible overflow-x-scroll scrollbar-none">
         {
           docSlots.map((data, index)=>{
             return(
-              <div key={index} onClick={()=> setslotIndex(index)} className={`rounded-full px-4 py-6 flex flex-col items-center cursor-pointer ${slotIndex === index ? "bg-primary text-white" : "border border-gray-500"}`}>
-                <p>{DaysOfWeek[data[0].dateTime.getDay()]}</p>
-                <p>{data[0].dateTime.getDate()}</p>
+              <div key={index} onClick={()=> setslotIndex(index)} className={`rounded-full px-2 py-4 md:px-4 md:py-6 flex flex-col items-center cursor-pointer ${slotIndex === index ? "bg-primary text-white" : "border border-gray-500"}`}>
+                <p className="md:text-lg text-sm">{DaysOfWeek[data[0].dateTime.getDay()]}</p>
+                <p className="md:text-lg text-sm">{data[0].dateTime.getDate()}</p>
               </div>
             )
           })
         }
       </div>
-     <div className="flex gap-3 overflow-x-scroll w-6/12 scrollbar-none">
+     <div className="flex gap-3 w-5/6 overflow-x-scroll scrollbar-none">
         {
           docSlots[slotIndex].map((data,index)=>{
             return(
-                <p key={index} onClick={()=> setslotTime(data.time)} className={`border border-gray-500 px-3 py-2 rounded-full cursor-pointer ${data.time === slotTime ? "bg-primary text-white" : "border border-gray-500"}`}>{data.time.toLowerCase()}</p>
+                <p key={index} onClick={()=> setslotTime(data.time)} className={`border border-gray-500 px-3 py-2 rounded-full cursor-pointer md:text-lg text-sm ${data.time === slotTime ? "bg-primary text-white" : "border border-gray-500"}`}>{data.time.toLowerCase()}</p>
             )
           })
         }
       </div>
-      <button className="bg-primary text-white px-8 py-4 rounded-full w-60">Book Appointment</button>
+      <button className="bg-primary text-white px-8 py-4 rounded-full w-60 md:text-lg text-sm">Book Appointment</button>
       </div>
      </div>
      <Relateddoctor docId={docinfo._id} speciality={docinfo.speciality}/>
